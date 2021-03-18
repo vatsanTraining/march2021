5231ce503ff972d4881fb7604edf1ef3211acefb
srivatsan <vatsank@gmail.com>
2021-03-18 15:21:18 +0530

    Payment Service Added

---
+++ payment-service/src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Customer {
+
+	int id;
+	String customerName;
+	String email;
+}
