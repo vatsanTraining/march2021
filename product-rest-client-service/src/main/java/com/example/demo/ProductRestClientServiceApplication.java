5fe0f39106423c0e530b1d80cb140c26ebf4849d
srivatsan <vatsank@gmail.com>
2021-03-19 14:09:12 +0530

    Update

--- product-rest-client-service/src/main/java/com/example/demo/ProductRestClientServiceApplication.java
+++ product-rest-client-service/src/main/java/com/example/demo/ProductRestClientServiceApplication.java
@@ -2,14 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.service.ClientService;
 
 @SpringBootApplication
 public class ProductRestClientServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductRestClientServiceApplication.class, args);
+	  ConfigurableApplicationContext ctx =	SpringApplication.run(ProductRestClientServiceApplication.class, args);
+	
+	
+	     ClientService service =
+	    		   ctx.getBean(ClientService.class);
+	     
+	     service.fetchItems();
+	     
+	     ctx.close();
+	
 	}
 
 	
