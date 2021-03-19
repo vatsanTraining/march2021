08b7bab3be9169f735e029c376ca8b3bda8e52e2
srivatsan <vatsank@gmail.com>
2021-03-19 11:58:21 +0530

    Update

--- product-rest-service/src/main/java/com/example/demo/respos/ItemRepository.java
+++ product-rest-service/src/main/java/com/example/demo/respos/ItemRepository.java
@@ -1,11 +1,23 @@
 package com.example.demo.respos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entity.Item;
+import java.util.*;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+	
+	// Custom Query Method Using  MethodNameQuery
+	public List<Item> findByName(String name);
+	
+	
+	// Custom Query Using @Query and Native sql query
+	@Query(value = "select * from ecomm_product where quantity>:qty",nativeQuery = true)
+	public List<Item> findItemByQuntityGt(@Param("qty") int srchqty);
+	
 	
 	
 }
