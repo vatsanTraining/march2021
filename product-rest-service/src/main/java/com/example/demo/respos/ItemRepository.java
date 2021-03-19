5a4fbca68cf2c71893742f97518c34dd9f2777c8
srivatsan <vatsank@gmail.com>
2021-03-19 09:55:22 +0530

    Updated Rest Service Example

---
+++ product-rest-service/src/main/java/com/example/demo/respos/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.respos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+	
+}
