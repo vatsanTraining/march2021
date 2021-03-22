fee7514c6966de76265c939fd609ace638217242
srivatsan <vatsank@gmail.com>
2021-03-22 15:46:46 +0530

    Update Call Returning Mono

--- product-review-service-client/src/main/java/com/example/demo/controllers/ClientController.java
+++ product-review-service-client/src/main/java/com/example/demo/controllers/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ClientController {
@@ -20,11 +21,26 @@ public class ClientController {
 	@GetMapping(path = "/client/reviews")
 	public String getReviews() {
 		
-		Flux<String> response =client.get().uri("lb://REVIEW-SERVICE/api/v1/reviews").retrieve().bodyToFlux(String.class);
+		Flux<String> response =
+				client.get().uri("lb://REVIEW-SERVICE/api/v1/reviews")
+				.retrieve()
+				.bodyToFlux(String.class);
 		
 		 response.subscribe(result -> this.reviews=result);
 		 
 		 return reviews;
 		
 	}
+	
+	
+	@GetMapping(path = "/client/v2/reviews")
+	public Mono<String> getReviewsAsMono() {
+		
+		    	return client.get().uri("lb://REVIEW-SERVICE/api/v1/reviews")
+				.retrieve()
+				.bodyToMono(String.class);
+		
+		
+	}
+	
 }
