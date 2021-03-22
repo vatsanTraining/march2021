132306312ff24b616fcbe0595bc47c3d03406804
srivatsan <vatsank@gmail.com>
2021-03-22 14:47:43 +0530

    Flux and Mono Exercise

--- product-review-service-client/src/main/java/com/example/demo/ProductReviewServiceClientApplication.java
+++ product-review-service-client/src/main/java/com/example/demo/ProductReviewServiceClientApplication.java
@@ -2,12 +2,35 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.model.CreditCard;
+import com.example.demo.services.CreditCardService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ProductReviewServiceClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductReviewServiceClientApplication.class, args);
+	ConfigurableApplicationContext	ctx=  SpringApplication.run(ProductReviewServiceClientApplication.class, args);
+	
+	
+	      CreditCardService service=     ctx.getBean(CreditCardService.class);
+	      
+	      
+	      Flux<CreditCard> cards = service.getCards();
+	      
+	      
+	         cards.log().subscribe(System.out::println);
+	         
+	         
+	         
+	         Mono<CreditCard> card = service.getCardByPos(1);
+	         
+	            card.subscribe(System.out::println);
+	
 	}
 
 }
--- product-review-service-client/src/main/java/com/example/demo/services/CreditCardService.java
+++ product-review-service-client/src/main/java/com/example/demo/services/CreditCardService.java
@@ -1,13 +1,32 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.*;
 import com.example.demo.model.*;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 @Service
 public class CreditCardService {
 
 	
+	@Autowired
 	private List<CreditCard> cardList;
 	
+	
+	
+	public Flux<CreditCard> getCards(){
+		
+		return Flux.fromIterable(this.cardList);
+	}
+	
+	
+	public Mono<CreditCard> getCardByPos(int pos){
+		
+		// check element exist;
+		
+		return Mono.just(cardList.get(pos));
+	}
 	
 }
