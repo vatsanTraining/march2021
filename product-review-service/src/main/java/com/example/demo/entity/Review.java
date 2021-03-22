1d4c321349711a14e186408acb8c34e9ef2566c9
srivatsan <vatsank@gmail.com>
2021-03-22 11:30:46 +0530

    Review -Entity Added

---
+++ product-review-service/src/main/java/com/example/demo/entity/Review.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "ecom_reviews")
+public class Review {
+
+	@Id
+	private String id;
+	private String title;
+	private String description;
+	private Integer score;
+	private String productId;
+}
