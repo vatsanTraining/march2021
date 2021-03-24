c610f205f0f455a7146ffa739dfaebc4933c0ed9
srivatsan <vatsank@gmail.com>
2021-03-24 16:14:30 +0530

    Update Spring Cloud Gateway

--- cloud-gateway/src/main/java/com/example/demo/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/example/demo/CloudGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -10,4 +13,17 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+                .route(p -> p.path("/api/v1/reviews/**")
+                .filters(f -> f.addRequestHeader("Hello", "World"))
+                .uri("lb://REVIEW-SERVICE"))
+                .build();
+
+		
+	}
 }
+
