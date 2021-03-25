a85f7f676e610b43c97630f95350a640e7e5e357
srivatsan <vatsank@gmail.com>
2021-03-25 10:20:46 +0530

    Circuit Breaker Retry

---
+++ resilence4j-retry/src/main/java/com/example/demo/Resilence4jRetryApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Resilence4jRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilence4jRetryApplication.class, args);
+	}
+
+	
+	@Bean
+	public RestTemplate template() {
+		
+		return new RestTemplate();
+	}
+}
---
+++ resilence4j-retry/src/main/java/com/example/demo/controllers/OrderController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controllers;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+	
+    private static final String ORDER_SERVICE = "orderService";
+    @Autowired
+    private RestTemplate restTemplate;
+
+   
+    private int attempts;
+    
+    @GetMapping("/orders")
+    @ResponseStatus(HttpStatus.OK)
+    @Retry(name=ORDER_SERVICE, fallbackMethod = "orderFallback")
+    public ResponseEntity<String> createOrder(){
+    	System.out.println("Attempted :"+ attempts++);
+        String response = restTemplate.getForObject("http://localhost:8081/item", String.class);
+        return new ResponseEntity<String>(response, HttpStatus.OK);
+    }
+    
+    
+    public ResponseEntity<String> orderFallback(Exception e){
+    	log.info(e.getMessage());
+    	attempts =1;
+        return new ResponseEntity<String>("Item service is down", HttpStatus.OK);
+
+    }
+}
+
+
